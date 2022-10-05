21d1a7505b716dcad5b97d912facce8df315bc19
Roberto Bosio <roberto.bosio98@gmail.com>
2022-10-05 15:48:38 +0200

    added query data

--- subgraphIsomorphism.hpp
+++ subgraphIsomorphism.hpp
@@ -1,7 +1,21 @@
+#include <ap_int.h>
+
 #include "Parameters.hpp"
 #include "queryVertex.hpp"
 #include "Trie.hpp"
 
-void subgraphIsomorphism(){
-	
+#define DEBUG 1
+void subgraphIsomorphism(
+        ap_uint<8> numQueryVertices,
+        ap_uint<8> *queryOffset,
+        ap_uint<8> *queryAdjacency,
+        ap_uint<8> *queryOrder,
+        ap_uint<32> *queryLabels){
+
+#if DEBUG
+    for(int g = 0; g < numQueryVertices; g++){
+        std::cout << (unsigned char)queryLabels[g] << std::endl;
+    }
+#endif
+
 }
--- subiso-test.cpp
+++ subiso-test.cpp
@@ -1,8 +1,76 @@
 #include <fstream>
+#include <ap_int.h>
+#include "subgraphIsomorphism.hpp"
 
 int main(){
 
-	/* Max 254 query vertices */
-	uint8_t numQueryVertices = 0;
-	
+    /* Query data structure */
+    ap_uint<8> numQueryVertices = 0;       
+    ap_uint<8> *qAdjacency; 
+    ap_uint<8> *qOffsets;
+    ap_uint<8> *qOrder;
+    ap_uint<32> *qLabels;
+
+    std::string fLine{};
+    std::cout << "Start" << std::endl;
+
+    /* Max 254 query nodes */
+    uint16_t numQueryAdj = 0;
+
+    /* Query files */
+    std::ifstream fQueryLab("data/queryLables.txt");
+    std::ifstream fQueryOff("data/queryOffsets.txt");
+    std::ifstream fQueryCol("data/queryColumns.txt");
+    std::ifstream fQueryOrd("data/queryOrder.txt");
+
+    std::getline(fQueryLab, fLine);
+    sscanf(fLine.c_str(), "%hhu\n", &numQueryVertices);
+    
+    qOrder = (ap_uint<8>*)malloc(numQueryVertices);
+    qLabels = (ap_uint<8>*)malloc(numQueryVertices);
+    qOffsets = (ap_uint<8>*)malloc(numQueryVertices + 1);
+
+    /* Store labels */
+    for(int count = 0; count < numQueryVertices; count++){    
+        std::getline(fQueryLab, fLine);
+        sscanf(fLine.c_str(), "%u %u", &node, &label);
+        qLabels[node] = label;
+    }
+
+    /* Store offsets */
+    getline(fQueryOff, fLine);
+    for(int node = 0; node < numQueryVertex + 1; node++){    
+        getline (fQueryOff, fLine);
+        sscanf(fLine.c_str(), "%hu", &qOffsets[node]);
+    }
+
+    /* Store adjancy array */
+    getline(fQueryCol, fLine);
+    sscanf(fLine.c_str(), "%hu", &numQueryAdj);
+    qAdjacency = (ap_uint<8>*)malloc(numQueryAdj);
+    for(int count = 0; count < numQueryAdj; count++){    
+        getline (fQueryCol, fLine);
+        sscanf(fLine.c_str(), "%hhu", &qAdjacency[count]);
+    }
+
+    /* Store matching order */
+    for(int count = 0; count < numQueryVertices; count++){    
+        getline (fQueryOrd, fLine);
+        sscanf(fLine.c_str(), "%u", &qOrder[count]);
+    }
+
+    fQueryLab.close();
+    fQueryOff.close();
+    fQueryCol.close();
+    fQueryOrd.close();
+
+    subgraphIsomorphism(
+            numQueryVertices,
+            qOffsets,
+            qAdjacency,
+            qOrder,
+            qLabels);
+
+    std::cout << "End" << std::endl;
+    return 0;	
 }
