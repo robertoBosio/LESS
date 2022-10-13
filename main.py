0571a6533daa78af1d6d89730c81f2148577eb59
Roberto Bosio <roberto.bosio98@gmail.com>
2022-10-13 11:56:00 +0200

    Added python script for verification

---
+++ main.py
@@ -0,0 +1,39 @@
+import re
+import networkx as nx
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    D = nx.read_edgelist("data/dataEdges.txt", nodetype=int, create_using=nx.DiGraph)
+    Q = nx.read_edgelist("data/queryEdges.txt", nodetype=int, create_using=nx.DiGraph)
+    label_dict = {}
+
+    fq = open("data/queryLabels.txt", "r")
+    fq.readline()
+    for line in fq:
+        node, label = line.split()
+        node = int(node)
+        label = int(label)
+        label_dict[node] = label
+    fq.close()
+    nx.set_node_attributes(Q, label_dict, "label")
+
+    label_dict.clear()
+    fq = open("data/dataLabels.txt", "r")
+    fq.readline()
+    for line in fq:
+        node, label = line.split()
+        node = int(node)
+        label = int(label)
+        label_dict[node] = label
+    fq.close()
+    nx.set_node_attributes(D, label_dict, "label")
+
+    GM = nx.algorithms.isomorphism.DiGraphMatcher(D, Q, node_match=nx.isomorphism.categorical_node_match("label", None))
+    s = GM.subgraph_isomorphisms_iter()
+    counter = 0
+    for sub in s:
+        counter = counter + 1
+    fo = open("data/golden.txt", "w")
+    print(counter, file=fo)
+    fo.close()
+
