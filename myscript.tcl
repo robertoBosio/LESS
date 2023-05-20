93f53239ead9491bc08b50aef90674b1691ed586
Roberto Bosio <roberto.bosio98@gmail.com>
2023-05-20 14:50:30 +0000

    Added solution with no preproc

--- myscript.tcl
+++ myscript.tcl
@@ -1,6 +1,10 @@
 # open_project -reset subgraphiso
+# open_project -reset subgraphiso-kria-nopreproc
 open_project -reset subgraphiso-kria
 # open_project subgraphiso
+
+# add_files "source-nopreproc/subisoWrap.cpp" -cflags "-I ./source-nopreproc"
+# add_files -tb "source-nopreproc/subiso-test.cpp data dataset" -cflags "-Wno-attributes -Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 add_files "source/subisoWrap.cpp" -cflags "-I ./source"
 add_files -tb "source/subiso-test.cpp data dataset" -cflags "-Wno-attributes -Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 
@@ -8,6 +12,7 @@ set_top subgraphIsomorphism
 
 open_solution "solution1" -flow_target vivado
 config_array_partition -complete_threshold 1
+config_interface -m_axi_latency=10
 # set_part {xczu3eg-sbva484-1-e}
 set_part {xck26-sfvc784-2LV-c}
 # set_part {xczu5eg-sfvc784-1-i}
