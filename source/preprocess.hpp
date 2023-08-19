2a5cf3ff638888ebd7e4a072e749a04c1af1d4aa
Roberto Bosio <roberto.bosio98@gmail.com>
2023-08-19 08:22:48 +0000

    Modifed preprocessing using URAM to store counters

--- source/preprocess.hpp
+++ source/preprocess.hpp
@@ -1173,60 +1173,64 @@ COUNT_EDGE_PER_BLOCK_LOOP:
           edge.range(LABELSRC_NODE + LAB_W - 1, LABELSRC_NODE);
         ap_uint<NODE_W> nodedst = edge.range(DST_NODE + NODE_W - 1, DST_NODE);
         ap_uint<NODE_W> nodesrc = edge.range(SRC_NODE + NODE_W - 1, SRC_NODE);
-
+        
         // Retrieve index of table with source as indexing vertex
         ap_uint<8> index0 = labelToTable[labelsrc][labeldst];
         // Retrieve index of table with destination as indexing vertex
         ap_uint<8> index1 = labelToTable[labeldst][labelsrc];
-        
-        if (index0 != 0) {
-            ap_uint<NODE_W> vertexIndexing;
-            ap_uint<8> index = index0 - 1;
-            vertexIndexing = nodesrc;
+        unsigned char step0 = 1; 
+        unsigned char step1 = 1; 
+        step0 = (index0 != 0) ? step0 : 0;
+        step1 = (index1 != 0) ? step1 : 0;
 
-            /* Compute indices for hash table */
-            ap_uint<LKP3_HASH_W> hash_out;
-            xf::database::details::hashlookup3_core<NODE_W>(
-                vertexIndexing,
-                hash_out);
+        /* Compute indices for hash table */
+        ap_uint<LKP3_HASH_W> hash_out0;
+        ap_uint<LKP3_HASH_W> hash_out1;
+        xf::database::details::hashlookup3_core<NODE_W>(nodesrc, hash_out0);
 
-            ap_uint<MAX_HASH_W> indexAdj = hash_out.range(MAX_HASH_W - 1, 0);
-            indexAdj = indexAdj.range(hash1_w - 1, 0);
-            
-            /* Compute inside which block the edge will finish, in the meanwhile also 
-            adjust the edge as indexing -> indexed, and also add the hash values */
-            auto address_intable = indexAdj.range(hash1_w - 1, hash1_w - block_per_table);
-            auto address = (1UL << block_per_table) * index + address_intable;
-            block_n_edges[address]++;
-#ifndef __SYNTHESIS__
-            assert(block_n_edges[address] < UINT32_MAX);
+        ap_uint<MAX_HASH_W> hashsrc = hash_out0.range(MAX_HASH_W - 1, 0);
+        hashsrc = hashsrc.range(hash1_w - 1, 0);
+
+        xf::database::details::hashlookup3_core<NODE_W>(nodedst, hash_out1);
+
+        ap_uint<MAX_HASH_W> hashdst = hash_out1.range(MAX_HASH_W - 1, 0);
+        hashdst = hashdst.range(hash1_w - 1, 0);
+
+        /* Compute inside which block the edge will finish, in the meanwhile
+        also adjust the edge as indexing -> indexed, and also add the hash
+        values */
+        unsigned short address_intable0 =
+          hashsrc.range(hash1_w - 1, hash1_w - block_per_table);
+        unsigned int address0 = (1UL << block_per_table) * (index0 - 1) + address_intable0;
+        unsigned short address_intable1 =
+          hashdst.range(hash1_w - 1, hash1_w - block_per_table);
+        unsigned int address1 = (1UL << block_per_table) * (index1 - 1) + address_intable1;
+
+#ifndef __SYNHTESIS__
+        address0 = (index0 == 0) ? 0 : address0;
+        address1 = (index1 == 0) ? 0 : address1;
 #endif
+
+        unsigned int counter0 = block_n_edges[address0];
+        unsigned int counter1 = block_n_edges[address1];
+
+        /* The case in which the nodes have the same labels and ends up in the same block 
+        will write the same counter two times */
+        if (address0 == address1 && index0 != 0 && index1 != 0){
+            step1 = step1 << 1;
+            step0 = step0 << 1;
         }
 
+        if (index0 != 0){
+            block_n_edges[address0] = counter0 + step0;
+        }
         if (index1 != 0) {
-            ap_uint<NODE_W> vertexIndexing;
-            ap_uint<8> index = index1 - 1;
-            vertexIndexing = nodedst;
-
-            /* Compute indices for hash table */
-            ap_uint<LKP3_HASH_W> hash_out;
-            xf::database::details::hashlookup3_core<NODE_W>(
-                vertexIndexing,
-                hash_out);
-
-            ap_uint<MAX_HASH_W> indexAdj = hash_out.range(MAX_HASH_W - 1, 0);
-            
-            indexAdj = indexAdj.range(hash1_w - 1, 0);
-            
-            /* Compute inside which block the edge will finish, in the meanwhile also 
-            adjust the edge as indexing -> indexed, and also add the hash values */
-            auto address_intable = indexAdj.range(hash1_w - 1, hash1_w - block_per_table);
-            auto address = (1UL << block_per_table) * index + address_intable;
-            block_n_edges[address]++;
-#ifndef __SYNTHESIS__
-            assert(block_n_edges[address] < UINT32_MAX);
-#endif
+            block_n_edges[address1] = counter1 + step1;
         }
+#ifndef __SYNTHESIS__
+        assert(block_n_edges[address0] < UINT32_MAX);
+        assert(block_n_edges[address1] < UINT32_MAX);
+#endif
     }
 }
 
@@ -1253,7 +1257,11 @@ storeEdgePerBlock(row_t* edge_buf,
 
 STORE_EDGE_BLOCK_LOOP:
     for (auto s = 0; s < numDataEdges; s++) {
+        /* We can remove this dependency since writing and reading are on
+        different portion of memory */
 #pragma HLS dependence variable = edge_buf type = inter direction = RAW false
+#pragma HLS dependence variable = block_n_edges type = intra false
+#pragma HLS pipeline II = 4
         row_t edge = edge_buf[dynfifo_space + s];
 
         ap_uint<LAB_W> labeldst =
@@ -1262,91 +1270,77 @@ STORE_EDGE_BLOCK_LOOP:
           edge.range(LABELSRC_NODE + LAB_W - 1, LABELSRC_NODE);
         ap_uint<NODE_W> nodedst = edge.range(DST_NODE + NODE_W - 1, DST_NODE);
         ap_uint<NODE_W> nodesrc = edge.range(SRC_NODE + NODE_W - 1, SRC_NODE);
-
+        
         // Retrieve index of table with source as indexing vertex
         ap_uint<8> index0 = labelToTable[labelsrc][labeldst];
         // Retrieve index of table with destination as indexing vertex
         ap_uint<8> index1 = labelToTable[labeldst][labelsrc];
+        unsigned char step0 = 1; 
+        unsigned char step1 = 1; 
+        step0 = (index0 != 0) ? step0 : 0;
+        step1 = (index1 != 0) ? step1 : 0;
+
+        /* Compute indices for hash table */
+        ap_uint<LKP3_HASH_W> hash_out0;
+        ap_uint<LKP3_HASH_W> hash_out1;
+        xf::database::details::hashlookup3_core<NODE_W>(nodesrc, hash_out0);
+
+        ap_uint<MAX_HASH_W> hashsrc = hash_out0.range(MAX_HASH_W - 1, 0);
+        hashsrc = hashsrc.range(hash1_w - 1, 0);
+
+        xf::database::details::hashlookup3_core<NODE_W>(nodedst, hash_out1);
+
+        ap_uint<MAX_HASH_W> hashdst = hash_out1.range(MAX_HASH_W - 1, 0);
+        hashdst = hashdst.range(hash1_w - 1, 0);
+
+        /* Compute inside which block the edge will finish, in the meanwhile
+        also adjust the edge as indexing -> indexed, and also add the hash
+        values */
+        unsigned short address_intable0 =
+          hashsrc.range(hash1_w - 1, hash1_w - block_per_table);
+        unsigned int address0 = (1UL << block_per_table) * (index0 - 1) + address_intable0;
+        unsigned short address_intable1 =
+          hashdst.range(hash1_w - 1, hash1_w - block_per_table);
+        unsigned int address1 = (1UL << block_per_table) * (index1 - 1) + address_intable1;
+#ifndef __SYNHTESIS__
+        address0 = (index0 == 0) ? 0 : address0;
+        address1 = (index1 == 0) ? 0 : address1;
+#endif
+        unsigned int offset0 = block_n_edges[address0];
+        unsigned int offset1 = block_n_edges[address1];
+
+        /* The case in which the nodes have the same labels and ends up in the same block 
+        will write the same edge two times, but with different vertex indexing */
+        if (address0 == address1 && index0 != 0 && index1 != 0){
+            step0 = step0 << 1;
+            offset1 += 1;
+        }
 
         if (index0 != 0) {
-            ap_uint<NODE_W> vertexIndexing, vertexIndexed;
-            ap_uint<8> index = index0 - 1;
-            vertexIndexing = nodesrc;
-            vertexIndexed = nodedst;
-
-            /* Compute indices for hash table */
-            ap_uint<LKP3_HASH_W> hash_out;
-            xf::database::details::hashlookup3_core<NODE_W>(
-                vertexIndexing,
-                hash_out);
-
-            ap_uint<MAX_HASH_W> indexAdj = hash_out.range(MAX_HASH_W - 1, 0);
-
-            xf::database::details::hashlookup3_core<NODE_W>(
-                vertexIndexed,
-                hash_out);
-
-            ap_uint<MAX_HASH_W> indexEdge = hash_out.range(MAX_HASH_W - 1, 0);
-            
-            indexAdj = indexAdj.range(hash1_w - 1, 0);
-            indexEdge = indexEdge.range(hash2_w - 1, 0);
-            
-            /* Compute inside which block the edge will finish, in the meanwhile also 
-            adjust the edge as indexing -> indexed, and also add the hash values */
-            auto address_intable = indexAdj.range(hash1_w - 1, hash1_w - block_per_table);
-            auto address = (1UL << block_per_table) * index + address_intable;
-            auto address_edge = block_n_edges[address];
             row_t table_edge;
-            table_edge.range(31, 0) = vertexIndexing;
-            table_edge.range(63, 32) = vertexIndexed;
-            table_edge.range(95, 64) = indexAdj;
-            table_edge.range(127, 96) = indexEdge;
-            edge_buf[address_edge] = table_edge;
-            block_n_edges[address]++;
-#ifndef __SYNTHESIS__
-            assert(block_n_edges[address] < UINT32_MAX);
-#endif
+            table_edge.range(31, 0) = nodesrc;
+            table_edge.range(63, 32) = nodedst;
+            table_edge.range(95, 64) = hashsrc;
+            table_edge.range(127, 96) = hashdst.range(hash2_w - 1, 0);
+            edge_buf[offset0] = table_edge;
+            block_n_edges[address0] = offset0 + step0;
         }
 
         if (index1 != 0) {
-            ap_uint<NODE_W> vertexIndexing, vertexIndexed;
-            ap_uint<8> index = index1 - 1;
-            vertexIndexing = nodedst;
-            vertexIndexed = nodesrc;
-
-            /* Compute indices for hash table */
-            ap_uint<LKP3_HASH_W> hash_out;
-            xf::database::details::hashlookup3_core<NODE_W>(
-                vertexIndexing,
-                hash_out);
-
-            ap_uint<MAX_HASH_W> indexAdj = hash_out.range(MAX_HASH_W - 1, 0);
-
-            xf::database::details::hashlookup3_core<NODE_W>(
-                vertexIndexed,
-                hash_out);
-
-            ap_uint<MAX_HASH_W> indexEdge = hash_out.range(MAX_HASH_W - 1, 0);
-            
-            indexAdj = indexAdj.range(hash1_w - 1, 0);
-            indexEdge = indexEdge.range(hash2_w - 1, 0);
-            
-            /* Compute inside which block the edge will finish, in the meanwhile also 
-            adjust the edge as indexing -> indexed, and also add the hash values */
-            auto address_intable = indexAdj.range(hash1_w - 1, hash1_w - block_per_table);
-            auto address = (1UL << block_per_table) * index + address_intable;
-            auto address_edge = block_n_edges[address];
             row_t table_edge;
-            table_edge.range(31, 0) = vertexIndexing;
-            table_edge.range(63, 32) = vertexIndexed;
-            table_edge.range(95, 64) = indexAdj;
-            table_edge.range(127, 96) = indexEdge;
-            edge_buf[address_edge] = table_edge;
-            block_n_edges[address]++;
-#ifndef __SYNTHESIS__
-            assert(block_n_edges[address] < UINT32_MAX);
-#endif
+            table_edge.range(31, 0) = nodedst;
+            table_edge.range(63, 32) = nodesrc;
+            table_edge.range(95, 64) = hashdst;
+            table_edge.range(127, 96) = hashsrc.range(hash2_w - 1, 0);
+            edge_buf[offset1] = table_edge;
+            block_n_edges[address1] = offset1 + step1;
         }
+
+
+#ifndef __SYNTHESIS__
+        assert(block_n_edges[address0] < UINT32_MAX);
+        assert(block_n_edges[address1] < UINT32_MAX);
+#endif
     }
 }
 
@@ -1378,6 +1372,7 @@ COUNT_OCCURENCIES_TOP_LOOP:
 
 INITIALIZE_URAM_LOOP:
         for (auto g = 0; g < (1UL << 14); g++){
+#pragma HLS pipeline II = 1
             block_counter[g] = 0;
         }
 
@@ -1439,7 +1434,7 @@ storeEdgesHTB(row_t* edge_buf,
     constexpr size_t IXD_HASH = 96;
     const unsigned int block_per_table = (1UL << (hash1_w + hash2_w - COUNTERS_PER_BLOCK));
     unsigned int block_counter[16384];
-#pragma HLS bind_storage variable=block_counter type=RAM_2P impl=URAM
+#pragma HLS bind_storage variable=block_counter type=RAM_T2P impl=URAM
 
     auto prev_offset = 0;
 STORE_EDGES_TOP_LOOP:
@@ -1548,7 +1543,7 @@ fillTablesURAM(row_t* edge_buf,
     const unsigned int block_per_table =
       (1UL << (hash1_w + hash2_w - COUNTERS_PER_BLOCK));
     unsigned long start_addr = 0;
-    unsigned int block_n_edges[2048];
+    unsigned int block_n_edges[2048] = {};
 #pragma HLS bind_storage variable = block_n_edges type = RAM_2P impl = BRAM
 
 #ifndef __SYNTHESIS__
@@ -1586,7 +1581,7 @@ COUNTER_TO_OFFSET_BLOCK_LOOP:
       dynfifo_space,
       numDataEdges,
       block_n_edges);
-
+    
     blockToHTB<NODE_W, LAB_W, LKP3_HASH_W, MAX_HASH_W, MAX_LABELS>(
       edge_buf, htb_buf, hash1_w, hash2_w, numTables, block_n_edges);
 
@@ -1596,13 +1591,13 @@ COUNTER_TO_OFFSET_BLOCK_LOOP:
 
     start_addr = (start_addr + (1UL << CACHE_WORDS_PER_LINE)) &
                  ~((1UL << CACHE_WORDS_PER_LINE) - 1);
-    auto prev_offset = 0;
+    unsigned int prev_offset = 0;
 STORE_EDGES_POINTER_LOOP:
     for (unsigned short ntb = 0; ntb < numTables; ntb++) {
         hTables0[ntb].start_edges = start_addr;
-        hTables0[ntb].n_edges =
-          block_n_edges[((ntb + 1) * block_per_table) - 1] - prev_offset;
-        prev_offset = block_n_edges[((ntb + 1) * block_per_table) - 1];
+        unsigned int offset = block_n_edges[((ntb + 1) * block_per_table) - 1];
+        hTables0[ntb].n_edges = offset - prev_offset;
+        prev_offset = offset;
         start_addr += (hTables0[ntb].n_edges >> (ROW_LOG - EDGE_LOG)) + 1;
         start_addr = (start_addr + (1UL << CACHE_WORDS_PER_LINE)) &
                      ~((1UL << CACHE_WORDS_PER_LINE) - 1);
